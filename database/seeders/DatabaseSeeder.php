9d5e386f90a05cc9f9b5e4bce27aa6adb00af854
Yeremia Alfa Dio <raikkonendio@gmail.com>
2022-01-16 17:36:14 +0700

    Database Seeder change bracket

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+// use App\Models\User;
+// use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UsersTableSeeder;
 use Database\Seeders\RolesTableSeeder;
+// use Illuminate\Support\Str;
+// use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +19,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
         $this->call(RolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
